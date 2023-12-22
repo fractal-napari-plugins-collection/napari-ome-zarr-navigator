7bdd9fe85e00aeed592b177076340b74d5520d8f
Fabio Steffen <35061965+fdsteffen@users.noreply.github.com>
2023-12-22 16:23:00 +0100

    Add tests for test_img_browser

---
+++ src/napari_ome_zarr_navigator/_tests/test_img_browser.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from napari_ome_zarr_navigator.img_browser import (
+    ImgBrowser,
+)
+
+
+def test_image_threshold_widget(make_napari_viewer):
+    viewer = make_napari_viewer()
+    layer = viewer.add_image(np.random.random((100, 100)))
+    my_widget = ImgBrowser(viewer)
