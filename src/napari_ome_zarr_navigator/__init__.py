d80440e49ad682f9e1e6fa9ff3c494e37367f853
Fabio Steffen <35061965+fdsteffen@users.noreply.github.com>
2023-12-22 18:39:41 +0100

    Rename file variable

--- src/napari_ome_zarr_navigator/__init__.py
+++ src/napari_ome_zarr_navigator/__init__.py
@@ -12,8 +12,8 @@ from .roi_loader import (
 )
 
 __all__ = ("ROILoader", "ImgBrowser")
-__file__ = Path(__file__)
-_PACKAGE_DIR = __file__.parents[2]
-_MODULE_DIR = __file__.parent
+FILE = Path(__file__).resolve()
+_PACKAGE_DIR = FILE.parents[2]
+_MODULE_DIR = FILE.parent
 _TEST_DIR = _MODULE_DIR.joinpath("_tests")
 _TEST_DATA_DIR = _PACKAGE_DIR.joinpath("test_data")
