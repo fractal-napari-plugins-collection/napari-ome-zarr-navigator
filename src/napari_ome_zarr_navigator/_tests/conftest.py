8c47f20a8567aec722ac4260623f956cd7aef834
jluethi <joel.luethi@uzh.ch>
2024-03-29 15:22:28 +0100

    Add pooch test data download, test fixture & github pooch caching

---
+++ src/napari_ome_zarr_navigator/_tests/conftest.py
@@ -0,0 +1,63 @@
+import os
+import shutil
+from pathlib import Path
+
+import pooch
+import pytest
+
+
+@pytest.fixture(scope="session")
+def testdata_path() -> Path:
+    TEST_DIR = Path(__file__).parent.parent.parent.parent
+    return TEST_DIR / "test_data/"
+
+
+@pytest.fixture(scope="session")
+def zenodo_zarr(testdata_path: Path) -> list[str]:
+    """
+    This takes care of multiple steps:
+
+    1. Download/unzip two Zarr containers (3D and MIP) from Zenodo, via pooch
+    2. Copy the two Zarr containers into tests/data
+    3. Modify the Zarrs in tests/data, to add whatever is not in Zenodo
+    """
+
+    # 1 Download Zarrs from Zenodo
+    DOI = "10.5281/zenodo.10424292"
+    DOI_slug = DOI.replace("/", "_").replace(".", "_")
+    rootfolder = testdata_path / DOI_slug
+
+    registry = {
+        "20200812-CardiomyocyteDifferentiation14-Cycle1.zarr.zip": None,
+        "20200812-CardiomyocyteDifferentiation14-Cycle1_mip.zarr.zip": None,
+    }
+    folders = [rootfolder / plate[:-4] for plate in registry]
+
+    base_url = f"doi:{DOI}"
+    POOCH = pooch.create(
+        pooch.os_cache("pooch") / DOI_slug,
+        base_url,
+        registry=registry,
+        retry_if_failed=10,
+        allow_updates=False,
+    )
+
+    for ind, file_name in enumerate(
+        [
+            "20200812-CardiomyocyteDifferentiation14-Cycle1.zarr",
+            "20200812-CardiomyocyteDifferentiation14-Cycle1_mip.zarr",
+        ]
+    ):
+        # 1) Download/unzip a single Zarr from Zenodo
+        file_paths = POOCH.fetch(
+            f"{file_name}.zip", processor=pooch.Unzip(extract_dir=file_name)
+        )
+        zarr_full_path = file_paths[0].split(file_name)[0] + file_name
+        print(zarr_full_path)
+        folder = folders[ind]
+
+        # 2) Copy the downloaded Zarr into tests/data
+        if os.path.isdir(str(folder)):
+            shutil.rmtree(str(folder))
+        shutil.copytree(Path(zarr_full_path) / file_name, folder)
+    return [str(f) for f in folders]
