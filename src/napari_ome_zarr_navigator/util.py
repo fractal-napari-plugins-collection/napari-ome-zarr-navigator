b8ceae2ff23dd2b4bfbfaf75ac8c145b603e671f
fdsteffen <35061965+fdsteffen@users.noreply.github.com>
2024-07-19 11:01:34 +0200

    Add function to add_feature_to_labels

--- src/napari_ome_zarr_navigator/util.py
+++ src/napari_ome_zarr_navigator/util.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 import string
+import napari
+from zarr.errors import PathNotFoundError
+import anndata as ad
+import pandas as pd
 
 
 def alpha_to_numeric(alpha: str) -> int:
@@ -27,3 +31,23 @@ def numeric_to_alpha(numeric: int, upper: bool = True) -> str:
         string.ascii_uppercase[numeric - 1]
     else:
         string.ascii_lowercase[numeric - 1]
+
+
+def add_features_to_labels(
+    zarr_url: str,
+    labels_layer: napari.layers.Labels,
+    feature_name: str = "regionprops",
+):
+    """Add features to napari labels layer
+
+    Args:
+        zarr_url: Path to the OME-ZARR
+        labels_layer: napari labels layer
+        feature_name: Folder name of the measured regionprobs features
+    """
+    try:
+        ann_tbl = ad.read_zarr(f"{zarr_url}/tables/{feature_name}/")
+        labels_layer.features = ann_tbl.to_df()
+        labels_layer.predictions = ann_tbl.obs
+    except PathNotFoundError:
+        pass
